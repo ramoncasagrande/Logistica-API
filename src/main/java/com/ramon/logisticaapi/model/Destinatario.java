0a78b5590a29d3b3973939509648da9acdbcdd7c
Ramon Casagrande <ramoncasagrande@hotmail.com>
2023-01-19 09:55:41 -0300

    Mapeamento Objeto Relacional de Entrega

--- src/main/java/com/ramon/logisticaapi/model/Destinatario.java
+++ src/main/java/com/ramon/logisticaapi/model/Destinatario.java
@@ -1,5 +1,6 @@
 package com.ramon.logisticaapi.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import lombok.Data;
 
@@ -7,10 +8,19 @@ import lombok.Data;
 @Embeddable
 public class Destinatario {
 
+    @Column(name = "destinatario_nome")
     private String nome;
+
+    @Column(name = "destinatario_logradouro")
     private String logradouro;
+
+    @Column(name = "destinatario_numero")
     private String numero;
+
+    @Column(name = "destinatario_complemento")
     private String complemento;
+
+    @Column(name = "destinatario_bairro")
     private String bairro;
     
 }
--- src/main/java/com/ramon/logisticaapi/model/Entrega.java
+++ src/main/java/com/ramon/logisticaapi/model/Entrega.java
@@ -5,15 +5,33 @@ import java.time.OffsetDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import lombok.Data;
 
 @Data
+@Entity
 public class Entrega {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne
     private Cliente cliente;
+
+    @Embedded
     private Destinatario destinatario;
-    private List<Ocorrencia> ocorrencias = new ArrayList<>();
+
+    //private List<Ocorrencia> ocorrencias = new ArrayList<>();
+
+    @Enumerated(EnumType.STRING)
     private StatusEntrega status;
     private BigDecimal taxa;
     private OffsetDateTime dataPedido;
