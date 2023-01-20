aef5d3fd981d1e3fc64c43cb5224441bf719e1bb
Ramon Casagrande <ramoncasagrande@hotmail.com>
2023-01-20 15:00:30 -0300

    Adicionados endpoints de listar e buscar entregas

--- src/main/java/com/ramon/logisticaapi/controller/EntregaController.java
+++ src/main/java/com/ramon/logisticaapi/controller/EntregaController.java
@@ -1,7 +1,12 @@
 package com.ramon.logisticaapi.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +14,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ramon.logisticaapi.model.Entrega;
+import com.ramon.logisticaapi.repository.EntregaRepository;
 import com.ramon.logisticaapi.services.SolicitacaoEntregaService;
 
 import jakarta.validation.Valid;
@@ -19,11 +25,25 @@ public class EntregaController {
 
     @Autowired
     private SolicitacaoEntregaService solicitacaoEnrtegaService;
+    @Autowired
+    private EntregaRepository entregaRepository;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Entrega solicitar(@Valid @RequestBody Entrega entrega){
+    public Entrega solicitar(@Valid @RequestBody Entrega entrega) {
         return solicitacaoEnrtegaService.solicitar(entrega);
     }
-    
+
+    @GetMapping
+    public List<Entrega> listar() {
+        return entregaRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Entrega> buscar(@PathVariable Long id) {
+        return entregaRepository.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
 }
--- src/main/java/com/ramon/logisticaapi/exception/NegocioException.java
+++ src/main/java/com/ramon/logisticaapi/exception/NegocioException.java
@@ -2,8 +2,6 @@ package com.ramon.logisticaapi.exception;
 
 public class NegocioException extends RuntimeException {
 
-    private static final long SerialVersionUID = 1L;
-
     public NegocioException(String mensagem){
         super(mensagem);
     }
